d03a0c557ce2f5e72715ed522d3684c4c8e758bb
Parad0x <james.i.binford@gmail.com>
2024-11-07 12:56:40 -0600

    Moving handler out of main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,16 @@
 mod chatgpt;
-mod config_test;
 mod config;
+mod handler;
+mod prelude;
 
-use tokio::net::TcpListener;
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::task;
+use crate::prelude::*;
 use tracing::{info, error, debug};
 use tracing_subscriber::{fmt, EnvFilter};
 use tracing_appender::rolling;
 use chatgpt::ChatGPT;
+use handler::handle_client;
 
-async fn handle_client(mut stream: tokio::net::TcpStream, message: String) {
-    let mut buffer = [0; 1024];
-    
-    loop {
-        match stream.read(&mut buffer).await {
-            Ok(0) => {
-                info!("Connection closed");
-                break;
-            }
-            Ok(n) => {
-                // Pass user input to ChatGPT, parse the GPT response and send it back to the user
-                // @TODO: Implement ChatGPT API
-                let received_data = String::from_utf8_lossy(&buffer[0..n]);
-                let response_message = format!("{}", message);
-                info!("Received data: {}", received_data);
-                info!("Response message: {}", response_message);
-                
-                if let Err(e) = stream.write_all(response_message.as_bytes()).await {
-                    println!("Failed to send data: {}", e);
-                    info!("Failed to write data.");
-                    break;
-                }
-            }
-            Err(e) => {
-                tracing::info!("Failed to read from stream: {}", e);
-                break;
-            }
-        }
-    }
-}
+
 
 async fn start_listener(addr: &str) -> tokio::io::Result<()> {
     let listener = TcpListener::bind(addr).await?;    
